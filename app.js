fbaed45d753bdc754ad7dc419caa210836cc39c7
jsramraj <jsramraj@gmail.com>
2020-01-04 12:40:41 +0530

    feat (none): upload file to the server

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const bodyParser = require('body-parser');
 
 const fileOperations = require('./file-operations');
 const clients = require('./client-module');
+const multer = require('./multer.config');
+
+const mkdirp = require('mkdirp');
+const path = require('path');
+var fs = require('fs');
 
 const server = http.createServer();
 const io = socket(server);
@@ -26,6 +31,14 @@ io.on('connection', client => {
             files.forEach(file => {
                 console.log(file.name, " ", file.size / 1000, "MB");
             });
+          } catch(err) {
+            console.error(err)
+          }
+    });
+    client.on('status', function(data) {
+        // console.log("Data: ", data);
+        try {
+            console.log("Data: ", data);
           } catch(err) {
             console.error(err)
           }
@@ -55,6 +68,34 @@ app.get('/commands/view', (req, res, next) => {
     return res.sendStatus(200);
 });
 
+var upload = multer.single('file');
+app.post('/fileupload',function(req,res){
+    console.log('fileupload started');
+    let directory = path.join('uploads');
+    upload(req,res,function(err) {
+        if(err) {
+            console.log('Error ', err);
+            return res.end("Error uploading file.");
+        }
+        console.log('File is uploaded to path ', req.file);
+        
+        fs.readFile(req.file.path, "utf8", function(err, data) {
+            console.log('Content ', data);
+        });
+
+        // res.end("File is uploaded");
+        res.end(req.file.path);
+    });
+});
+
 app.listen(8080, () => {
     console.log("Server is listening on port", 8080);
+
+    mkdirp.sync(path.join(__dirname, '..', 'uploads'), (err) => {
+        if (err) {
+            console.log('Error creating directory', err);
+        } else {
+            console.log('Successfully created uploads directory');
+        }
+      });
 });
